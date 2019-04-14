57b3a1aa49ce84dd14d250ffca50989b93894d1f
conneryfrommi6 <47834023+conneryfrommi6@users.noreply.github.com>
2019-04-14 20:50:34 +0300

    Update 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,11 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   const year = date.getFullYear();
+    if (year % 4 !== 0) return false;
+    if (year % 100 !== 0) return true;
+    if (year % 400 !== 0) return false;
+    return true;
 }
 
 
@@ -76,7 +80,9 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   const dateresult = new Date("Sun, 17 May 1998 00:00:00 GMT+00");
+   dateresult.setMilliseconds(endDate.getTime() - startDate.getTime());
+   return dateresult.toISOString().slice(11, 23);
 }
 
 
@@ -94,7 +100,14 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   const minutes = date.getUTCMinutes();
+   const hours = date.getUTCHours();
+   let ugl = 0.5 * (60 * hours + minutes) - 6 * minutes;
+   while ( ugl > 180) {
+     ugl -= 360;
+     ugl = Math.abs(ugl);
+   }
+   return ugl * Math.PI / 180; 
 }
 
 
